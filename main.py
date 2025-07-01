e0c3a0465832acd50cf3e03fee775ae7a885fa4e
yamilobt8 <yamilo12345678910@gmail.com>
2025-07-01 17:00:50 +0100

    Implemented email verification process

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from collections import Counter
 import os
+from selenium.common.exceptions import NoSuchElementException
 from dotenv import load_dotenv
 
 def get_followers_followings_count():
@@ -46,6 +47,7 @@ def scroll(followers_followings_count):
         print(f'{i}/{times_to_scorll}')
         driver.execute_script("""arguments[0].scrollTop = arguments[0].scrollHeight;""", scrollable_div)
         sleep(1.5)
+    driver.execute_script("""arguments[0].scrollTo({top: 0,behavior: 'smooth'})""", scrollable_div)
 
 def get(followers_followings_count):
 
@@ -70,12 +72,25 @@ def unfollow_unmutuals(followers_followgins_count, unmutuals):
         user_href = user.find_element(By.TAG_NAME, 'a').get_attribute('href')
         if user_href in unmutuals:
             user.find_element(By.TAG_NAME, 'button').click()
-            print(f'{user_href[25:-1]} has been unfollowed')
+            unfollow_btn = driver.find_element(By.XPATH, ".//button[normalize-space(text())='Unfollow']")
+            unfollow_btn.click()
+            print(f'{user_href[26:-1]} has been unfollowed')
+            sleep(1.5)
 
 def close():
     close_button = driver.find_elements(By.TAG_NAME, 'button')[1]
     close_button.click()
     
+    
+def login():
+    username = driver.find_element(By.CSS_SELECTOR, 'input[aria-label="Phone number, username, or email"]')
+    username.send_keys(os.getenv('USERNAME'))
+
+    password = driver.find_element(By.CSS_SELECTOR, 'input[aria-label="Password"]')
+    password.send_keys(os.getenv("PASSWORD"))
+
+    log_in = driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
+    log_in.click()
 
 load_dotenv()
 
@@ -86,32 +101,66 @@ driver.get("https://www.instagram.com/")
 
 sleep(5)
 
-username = driver.find_element(By.CSS_SELECTOR, 'input[aria-label="Phone number, username, or email"]')
-username.send_keys(os.getenv('USERNAME'))
-
-password = driver.find_element(By.CSS_SELECTOR, 'input[aria-label="Password"]')
-password.send_keys(os.getenv("PASSWORD"))
-
-log_in = driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
-log_in.click()
+login()
 
 sleep(9)
 
-back_up_codes = driver.find_element(By.XPATH, '//button[.//div[text()="backup codes"]]')
-back_up_codes.click()
+try:
+    back_up_codes = driver.find_element(By.XPATH, '//button[.//div[text()="backup codes"]]')
+    back_up_codes.click()
+    sleep(1)
+    verification_code = driver.find_element(By.TAG_NAME, 'input')
+    verification_code.send_keys(os.getenv("BACKUP_CODE"))
+    confirm = driver.find_element(By.XPATH, '//button[contains(., "Confirm")]')
+    confirm.click()
+except NoSuchElementException:
+    print("No 2FA detected")
 
-sleep(1)
 
-verification_code = driver.find_element(By.TAG_NAME, 'input')
-verification_code.send_keys(os.getenv("BACKUP_CODE"))
 
-confirm = driver.find_element(By.XPATH, '//button[contains(., "Confirm")]')
-confirm.click()
+def confirm_error(spans):
+    for span in spans:
+        if span.text == 'This code doesn’t work. Check it’s correct or try a new one.':
+            return True
+    return False
 
+def clear_input(input):
+    input.send_keys(Keys.CONTROL + "a")
+    input.send_keys(Keys.BACKSPACE)
+    
+    
+def confirm_login(spans, error=False):
+    message = 'This code doesn’t work. Check it’s correct or try a new one: ' if error else 'Enter the code sented to your email: '
+    code = input(message)
+    code_input = driver.find_element(By.TAG_NAME, 'input')
+    while len(code) != 6 or not code.isdigit():
+        code = input('THE confirmation code should contain 6 digits: ')
+    clear_input(code_input)
+    code_input.send_keys(code)
+    continue_btn = driver.find_element(By.XPATH, '//span[text()="Continue"]')
+    continue_btn.click()
+    sleep(2)
+    spans = driver.find_elements(By.TAG_NAME, 'span')
+    if confirm_error(spans):
+        confirm_login(spans, error=True)
+    else:
+        print('logged_in succesfully')
+    
+
+spans = driver.find_elements(By.TAG_NAME, 'span')
+label = driver.find_element(By.TAG_NAME, 'label').text == 'Code'
+confiramtion_needed = ['Check your email' in span.text for span in spans]
+if any(confiramtion_needed) and label:
+    confirm_login(spans)
+    
+        
+        
 sleep(19)
-
-not_now = driver.find_element(By.XPATH, "//div[text()='Not now']")
-not_now.click()
+try:
+    not_now = driver.find_element(By.XPATH, "//div[text()='Not now']")
+    not_now.click()
+except NoSuchElementException:
+    pass
 
 account_username = os.getenv('USERNAME')
 if account_username:
@@ -149,65 +198,7 @@ close()
         
 unmutuals = [user_link for user_link in followings_list if user_link not in followers_list]
 
-# for unmutual in unmutuals[:5]:
-#     driver.get(unmutual)
-    
-        
 
 followings.click()
 unfollow_unmutuals(followings_count, unmutuals)
 
-# i need to add the scroll
-# followings_div = driver.execute_script("""return document.querySelector('[style="display: flex; flex-direction: column; padding-bottom: 0px; padding-top: 0px; position: relative;"]');""")
-
-# child_elements = followings_div.find_elements(By.XPATH, './/*')
-
-# buttons_count = 0
-# unfollow_buttons = []
-
-
-# for child in child_elements:
-#     tag_name = child.tag_name
-#     if tag_name == 'button':
-#         buttons_count += 1
-#         unfollow_buttons.append(child)
-        
-# for i, following in enumerate(followings_list):
-#     if following in unmutuals:
-#         unfollow_buttons[i].click()
-#         sleep(1)
-#         unfollow()
-        
-    
-      
-# def unfollow():
-#     try:
-#         confirm_button = WebDriverWait(driver, 5).until(
-#             EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="Unfollow"]'))
-#         )
-#         confirm_button.click()
-#     except Exception:
-#         print('button not found')  
-#     sleep(2)
-
-# buttons = driver.find_elements(By.TAG_NAME, 'button')
-# print(len(buttons))
-
-
-
-# print(len(unfollow_buttons))
-# print(buttons_count)
-
-
-# buttons = driver.find_elements(By.TAG_NAME, 'button')
-# unfollowing_buttons = []
-# for button in buttons:
-#     if button.text == 'Following':
-#         unfollowing_buttons.append(button)
-        
-# print(len(unfollowing_buttons))
-
-# for i, following in enumerate(followings_list):
-#     if following in unmutuals:
-#         unfollowing_buttons[i].click()
-#         unfollow()
